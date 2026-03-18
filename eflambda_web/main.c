783f13e19e3b92de1aba05d6023869716ad81aea
etherevert <etherevert@gmail.com>
2026-03-18 18:16:34 +0300

    simple test

--- eflambda_web/main.c
+++ eflambda_web/main.c
@@ -1,9 +1,15 @@
 #include "lambda.h"
 
+#include <stdio.h>
+
 int main() {
     VarLib *lib = NULL;
-    char *src = "Id = \\x.x";
-    Term* term = term_parse(&src, &lib);
+    char *str = "Id = \\x.x";
+    Term *term = term_parse(&str, &lib);
+
+    char buf[1024];
+    term_display(buf, sizeof(buf), term);
+    printf("%s\n", buf);
+
     return 0;
 }
-

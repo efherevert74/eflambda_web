5b6aeeca5a621271c67d3bb3ad8bb17715ba5bae
etherevert <etherevert@gmail.com>
2026-03-18 21:09:39 +0300

    make basic wrappers to c funcs

--- eflambda_web/main.c
+++ eflambda_web/main.c
@@ -1,15 +1,24 @@
 #include "lambda.h"
+#include "lambda_std.h"
 
-#include <stdio.h>
+VarLib *lib = NULL;
+
+char* term_display_wrapper(Term *term) {
+    int buf_len = term_display(NULL, 0, term);
+    char *str = malloc(buf_len);
+    term_display(str, buf_len, term);
+    return str;
+}
+
+Term* term_parse_wrapper(char* str) {
+    return term_parse(&str, &lib);
+}
+
+bool term_reduce_wrapper(Term *term, bool lazy) {
+    return term_reduce(term, &lib, lazy);
+}
 
 int main() {
-    VarLib *lib = NULL;
-    char *str = "Id = \\x.x";
-    Term *term = term_parse(&str, &lib);
-
-    char buf[1024];
-    term_display(buf, sizeof(buf), term);
-    printf("%s\n", buf);
-
+    fill_std_lib(&lib);
     return 0;
 }

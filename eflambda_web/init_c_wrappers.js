eb02ebf0267c7896d52f47659423e18be9aca468
etherevert <etherevert@gmail.com>
2026-03-20 00:17:30 +0300

    add exporting VarLib to JS

--- eflambda_web/init_c_wrappers.js
+++ eflambda_web/init_c_wrappers.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-let c_term_parse, c_term_reduce, c_term_free, c_term_display;
+let c_var_lib_get_len, c_var_lib_get_term, c_var_lib_get;
+let c_term_parse, c_term_reduce, c_term_free, c_term_display, c_term_is_inv;
 Module.onRuntimeInitialized = () => {
+    c_term_free = Module.cwrap("term_free", "void", ["number"]);
     c_term_parse = Module.cwrap("term_parse_wrapper", "number", ["string"]);
     c_term_reduce = Module.cwrap("term_reduce_wrapper", "bool", [
         "number",
         "bool",
     ]);
-    c_term_free = Module.cwrap("term_free", "void", ["number"]);
+    c_term_is_inv = Module.cwrap("term_is_inv", "bool", ["number"]);
     c_term_display = (term) => {
         // another layer of wrapping for automatic conversion between c string
         // and js string
@@ -16,10 +18,52 @@ Module.onRuntimeInitialized = () => {
                 "number",
             ]);
         }
+
+        if (c_term_is_inv(term)) {
+            return "Invalid lambda term.";
+        }
+
         let str_ptr = c_term_display.f(term);
         let str = Module.UTF8ToString(str_ptr);
         str = str.replaceAll("\\", "λ");
         Module._free(str_ptr);
         return str;
+    };
+
+    // c_var_lib_get_term = Module.cwrap("var_lib_get_term", "number", [
+    //     "number",
+    // ]);
+    c_var_lib_get = () => {
+        if (
+            c_var_lib_get.get_lib === undefined ||
+            c_var_lib_get.get_len === undefined
+        ) {
+            c_var_lib_get.get_lib = Module.cwrap("var_lib_get", "number", []);
+            c_var_lib_get.get_len = Module.cwrap(
+                "var_lib_get_len",
+                "number",
+                [],
+            );
+        }
+        let lib = c_var_lib_get.get_lib();
+        let len = c_var_lib_get.get_len();
+        const sizeof_varlib = 16; // char* and Term*
+        const offsetof_term = 4; // sizeof(char*)
+
+        let res = {};
+        for (
+            let lib_i = lib;
+            lib_i < lib + len * sizeof_varlib;
+            lib_i += sizeof_varlib
+        ) {
+            let key_ptr = Module.HEAP32[lib_i >> 2];
+            let term_ptr = lib_i + offsetof_term;
+
+            let key = Module.UTF8ToString(key_ptr);
+            let term_str = c_term_display(term_ptr);
+
+            res[key] = term_str;
+        }
+        return res;
     };
 };
--- eflambda_web/main.c
+++ eflambda_web/main.c
@@ -3,20 +3,28 @@
 
 VarLib *lib = NULL;
 
-char* term_display_wrapper(Term *term) {
+int var_lib_get_len() { return shlen(lib); }
+
+Term *var_lib_get_term(int idx) { return term_copy(&lib[idx].value); }
+
+VarLib* var_lib_get() {
+    return lib;
+}
+
+char *term_display_wrapper(Term *term) {
     int buf_len = term_display(NULL, 0, term);
     char *str = malloc(buf_len);
     term_display(str, buf_len, term);
     return str;
 }
 
-Term* term_parse_wrapper(char* str) {
-    return term_parse(&str, &lib);
-}
+Term *term_parse_wrapper(char *str) { return term_parse(&str, &lib); }
 
 bool term_reduce_wrapper(Term *term, bool lazy) {
     return term_reduce(term, &lib, lazy);
 }
+
+bool term_is_inv(Term *term) { return term->type == TInv; }
 
 int main() {
     fill_std_lib(&lib);
